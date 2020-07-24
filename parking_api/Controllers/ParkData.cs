5ec191bc6696e939fa16dcf83bdaf5aced39110b
steven9587 <steven9587@gmail.com>
2020-07-24 09:37:46 +0800

    20200724 api_clear

---
+++ parking_api/Controllers/ParkData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace parking_api.Controllers
+{
+    public class ParkData
+    {
+        public float Twd97X { get; set; }
+        public float Twd97Y { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+        public string Address { get; set; }
+        public string ServiceTime { get; set; }
+        public string PayEx { get; set; }
+        public string TotalCar { get; set; }
+        public string TotalMotor { get; set; }
+        public string Summary { get; set; }
+        public string Id { get; set; }
+        public string Tel { get; set; }
+        public string UpdateTime { get; set; }
+    }
+}
--- parking_api/Controllers/ParkinginfoController.cs
+++ parking_api/Controllers/ParkinginfoController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace parking_api.Controllers
@@ -11,9 +8,9 @@ namespace parking_api.Controllers
     public class ParkinginfoController : ApiController
     {
         //利用area搜尋的API
-        public List<DbPark> Get(string area)
+        public List<ParkData> Get(string area)
         {
-            List<DbPark> conditionPark = new List<DbPark>();
+            List<ParkData> conditionPark = new List<ParkData>();
             SqlConnection conn = new SqlConnection("data source=LAPTOP-9CB9PVPB\\SQLEXPRESS; initial catalog = SYSTEX; user id = sa; password = 0000");
             conn.Open();
             string strSelect = "SELECT * FROM Parking where area ='" + area + "'";
@@ -24,7 +21,7 @@ namespace parking_api.Controllers
                 while (dr.Read())
                 {
                     //將所讀取到的該筆資料存入list中
-                    conditionPark.Add(new DbPark()
+                    conditionPark.Add(new ParkData()
                     {
                         Twd97X = (float)Convert.ToDouble(dr[0]),
                         Twd97Y = (float)Convert.ToDouble(dr[1]),
@@ -50,9 +47,9 @@ namespace parking_api.Controllers
         }
 
         //利用經度、緯度、及距離搜尋的API
-        public List<DbPark> Get(string lat, string lng, float distance)
+        public List<ParkData> Get(string lat, string lng, float distance)
         {
-            List<DbPark> conditionPark = new List<DbPark>();
+            List<ParkData> conditionPark = new List<ParkData>();
             SqlConnection conn = new SqlConnection("data source=LAPTOP-9CB9PVPB\\SQLEXPRESS; initial catalog = SYSTEX; user id = sa; password = 0000");
             conn.Open();
             string strSelect = "SELECT * FROM Parking;";
@@ -65,7 +62,7 @@ namespace parking_api.Controllers
                     if (distance >= DistanceOfTwoPoints(Convert.ToSingle(lat), Convert.ToSingle(lng), (float)Convert.ToDouble(dr[0]), (float)Convert.ToDouble(dr[1])))
                     {
                         //將所讀取到且符合距離的該筆資料存入list中
-                        conditionPark.Add(new DbPark()
+                        conditionPark.Add(new ParkData()
                         {
                             Twd97X = (float)Convert.ToDouble(dr[0]),
                             Twd97Y = (float)Convert.ToDouble(dr[1]),
@@ -104,24 +101,5 @@ namespace parking_api.Controllers
             s = (float)(Math.Round(s * 10000) / 10000);
             return s;
         }
-
-        public class DbPark
-        {
-            public float Twd97X { get; set; }
-            public float Twd97Y { get; set; }
-            public string Name { get; set; }
-            public string Area { get; set; }
-            public string Address { get; set; }
-            public string ServiceTime { get; set; }
-            public string PayEx { get; set; }
-            public string TotalCar { get; set; }
-            public string TotalMotor { get; set; }
-            public string Summary { get; set; }
-            public string Id { get; set; }
-            public string Tel { get; set; }
-            public string UpdateTime { get; set; }
-        }
-
-
     }
 }
